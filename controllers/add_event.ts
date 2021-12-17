f5c75bb35adb8ffc56ef618dfd866e9186bd99e8
JarSeal <kai.forsman@gmail.com>
2021-12-17 12:30:37 +0800

    Add jest igore patterns

--- controllers/add_event.ts
+++ controllers/add_event.ts
@@ -9,7 +9,7 @@ addEventRouter.post('/', async (request, response) => {
     const body = request.body;
 
     // Validate the name and dates
-    const nameValidationError = validateName(body.name.trim());
+    const nameValidationError = validateName(body.name);
     if(nameValidationError) {
         logger.log('Name validation backend error.', nameValidationError); // Validation errors should be caught in frontend, so this error should be logged
         response.json(nameValidationError);
@@ -38,22 +38,22 @@ addEventRouter.post('/', async (request, response) => {
     }
 });
 
-const validateName = (name) => {
+const validateName = (name: string) => {
     const nameConfig = config.EVENT_VALIDATION.name;
 
-    if(!name || !name.length) {
+    if(!name || !name.trim().length) {
         return {
             errorMsg: 'An event has to have the name attribute.',
             errorField: 'name',
             errorCode: 'nameIsRequired',
         };
-    } else if(name.length < nameConfig.minlength) {
+    } else if(name.trim().length < nameConfig.minlength) {
         return {
             errorMsg: `Name has to be at least ${nameConfig.minlength} characters.`,
             errorField: 'name',
             errorCode: 'nameTooShort'
         };
-    } else if(name.length > nameConfig.maxlength) {
+    } else if(name.trim().length > nameConfig.maxlength) {
         return {
             errorMsg: `Name cannot be more than ${nameConfig.maxlength} characters.`,
             errorField: 'name',
@@ -64,7 +64,7 @@ const validateName = (name) => {
     return null; // No errors
 };
 
-const validateDates = (dates) => {
+const validateDates = (dates: [string]) => {
     if(!dates) return null;
 
     for(let i=0; i<dates.length; i++) {
@@ -80,7 +80,7 @@ const validateDates = (dates) => {
     return null; // No errors
 };
 
-const isValidDate = (dateString) => {
+const isValidDate = (dateString: string) => {
     const regEx = /^\d{4}-\d{2}-\d{2}$/;
     if(!dateString.match(regEx)) return false;  // Invalid format
     const d = new Date(dateString);
---
+++ tests/01_list_and_add_events.test.ts
@@ -0,0 +1,67 @@
+import mongoose from 'mongoose';
+import supertest from 'supertest';
+import app from './../app';
+import Event from './../models/event';
+
+const api = supertest(app);
+
+beforeAll(async () => {
+    jest.setTimeout(10000);
+    await Event.deleteMany({});
+});
+
+console.log('HERE1');
+
+describe('API tests for list and add events', () => {
+    it('no events should be found', async () => {
+        const response = await api.get('/api/v1/event/list');
+        expect(response.body.length).toBe(0);
+    });
+
+    it('three events should be added', async () => {
+        const event1 = await api.post('/api/v1/event').send({ name: 'My new event number 1' });
+        const event2 = await api.post('/api/v1/event').send({ name: 'My new event number 2', dates: ['2021-01-05'] });
+        const event3 = await api.post('/api/v1/event').send({ name: 'My new event number 3', dates: ['2021-01-05', '2021-01-06', '2021-01-09'] });
+        expect(event1.body.id.length === 24).toBe(true);
+        expect(event2.body.id.length === 24).toBe(true);
+        expect(event3.body.id.length === 24).toBe(true);
+    });
+
+    it('three events should be found', async () => {
+        const response = await api.get('/api/v1/event/list');
+        expect(response.body.length).toBe(3);
+    });
+
+    it('event addition should fail with no name given', async () => {
+        const event1 = await api.post('/api/v1/event').send({ name: '' });
+        const event2 = await api.post('/api/v1/event').send({});
+        expect(event1.body.errorCode === 'nameIsRequired' && event2.body.errorCode === 'nameIsRequired').toBe(true);
+    });
+
+    it('event addition should fail with too short name given', async () => {
+        const event1 = await api.post('/api/v1/event').send({ name: 'a' });
+        expect(event1.body.errorCode === 'nameTooShort').toBe(true);
+    });
+
+    it('event addition should fail with too long name given', async () => {
+        let longName = '';
+        for(let i=0; i<2000; i++) { longName += 'a'; }
+        const event1 = await api.post('/api/v1/event').send({ name: longName });
+        expect(event1.body.errorCode === 'nameTooLong').toBe(true);
+    });
+
+    it('event addition should fail with invalid date formats and dates', async () => {
+        const event1 = await api.post('/api/v1/event').send({ name:'Some name 1', dates: [''] });
+        const event2 = await api.post('/api/v1/event').send({ name:'Some name 2', dates: ['fdsafdsafdsa'] });
+        const event3 = await api.post('/api/v1/event').send({ name:'Some name 3', dates: ['2013-40-12'] });
+        const event4 = await api.post('/api/v1/event').send({ name:'Some name 4', dates: ['2013-02-29'] }); // Was not a leap year
+        expect(event1.body.errorCode === 'invalidDate').toBe(true);
+        expect(event2.body.errorCode === 'invalidDate').toBe(true);
+        expect(event3.body.errorCode === 'invalidDate').toBe(true);
+        expect(event4.body.errorCode === 'invalidDate').toBe(true);
+    });
+});
+
+afterAll(async () => {
+    await mongoose.connection.close();
+});
