1153ce16ba1066121c1ef90a2ef29b29e48c387c
JarSeal <kai.forsman@gmail.com>
2021-12-17 11:00:33 +0800

    New event data validation

--- controllers/add_event.ts
+++ controllers/add_event.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import logger from '../utils/logger';
+import config from './../utils/config';
+import logger from './../utils/logger';
 import Event from './../models/event';
 
 const addEventRouter = express.Router();
@@ -8,17 +9,23 @@ addEventRouter.post('/', async (request, response) => {
     const body = request.body;
 
     // Validate the name and dates
-    if(!body.name || !body.name.trim().length) {
-        response.json({ errorMsg: 'An event has to have the name attribute.', errorField: 'name', errorCode: 'nameIsRequired' });
+    const nameValidationError = validateName(body.name.trim());
+    if(nameValidationError) {
+        logger.log('Name validation backend error.', nameValidationError); // Validation errors should be caught in frontend, so this error should be logged
+        response.json(nameValidationError);
         return;
-    } else if(body.name.trim().length < 3) {
-        response.json({ errorMsg: `Name has to be at least ${'3'} characters.`, errorField: 'name', errorCode: 'nameTooShort' });
+    }
+    const datesValidationError = validateDates(body.dates);
+    if(datesValidationError) {
+        logger.log('Data format validation backend error.', datesValidationError); // Validation errors should be caught in frontend, so this error should be logged
+        response.json(datesValidationError);
         return;
     }
     
+    // No errors, try saving the data to DB
     try {
         const event = new Event({
-            name: body.name,
+            name: body.name.trim(),
             dates: body.dates || [],
         });
         const savedEvent = await event.save();
@@ -30,5 +37,56 @@ addEventRouter.post('/', async (request, response) => {
         response.status(500).json({ errorMsg: 'Could not save event.' });
     }
 });
+
+const validateName = (name) => {
+    const nameConfig = config.EVENT_VALIDATION.name;
+
+    if(!name || !name.length) {
+        return {
+            errorMsg: 'An event has to have the name attribute.',
+            errorField: 'name',
+            errorCode: 'nameIsRequired',
+        };
+    } else if(name.length < nameConfig.minlength) {
+        return {
+            errorMsg: `Name has to be at least ${nameConfig.minlength} characters.`,
+            errorField: 'name',
+            errorCode: 'nameTooShort'
+        };
+    } else if(name.length > nameConfig.maxlength) {
+        return {
+            errorMsg: `Name cannot be more than ${nameConfig.maxlength} characters.`,
+            errorField: 'name',
+            errorCode: 'nameTooLong'
+        };
+    }
+
+    return null; // No errors
+};
+
+const validateDates = (dates) => {
+    if(!dates) return null;
+
+    for(let i=0; i<dates.length; i++) {
+        if(!isValidDate(dates[i])) {
+            return {
+                errorMsg: 'Date is not in a valid format.',
+                errorField: 'dates',
+                errorCode: 'invalidDate',
+            };
+        }
+    }
+
+    return null; // No errors
+};
+
+const isValidDate = (dateString) => {
+    const regEx = /^\d{4}-\d{2}-\d{2}$/;
+    if(!dateString.match(regEx)) return false;  // Invalid format
+    const d = new Date(dateString);
+    const dNum = d.getTime();
+    if(!dNum && dNum !== 0) return false; // NaN value, Invalid date
+    return d.toISOString().slice(0,10) === dateString;
+};
 
 export default addEventRouter;
