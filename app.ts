d454a9164ac2611cf8e5611ab23262b4880f4378
JarSeal <kai.forsman@gmail.com>
2021-12-17 13:34:09 +0800

    Baseurl refactoring

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import listEventsRouter from './controllers/list_events';
 import addEventRouter from './controllers/add_event';
+import showEventRouter from './controllers/show_event';
 import middleware from './utils/middleware';
 import logger from './utils/logger';
 
@@ -24,8 +25,10 @@ const app = express();
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.use('/api/v1/event/list', listEventsRouter);
-app.use('/api/v1/event', addEventRouter);
+const baseUrl = '/api/v1/event';
+app.use(baseUrl, listEventsRouter);
+app.use(baseUrl, showEventRouter);
+app.use(baseUrl, addEventRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
