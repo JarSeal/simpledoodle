abad873de54ffe29caf1cfd999121665d935ed51
JarSeal <kai.forsman@gmail.com>
2021-12-17 10:35:30 +0800

    Add event validation data to config

--- utils/config.ts
+++ utils/config.ts
@@ -1,6 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+const EVENT_VALIDATION = {
+    name: {
+        minlength: 3,
+        maxlength: 6,
+    },
+    date: {
+        regex: /^\d{4}-\d{2}-\d{2}$/,
+    },
+};
+
 const PORT = process.env.PORT;
 let MONGODB_URI = process.env.MONGODB_URI;
 
@@ -11,6 +21,7 @@ if(process.env.NODE_ENV === 'test') {
 const config = {
     MONGODB_URI,
     PORT,
+    EVENT_VALIDATION,
 };
 
 export default config;
